f17d78965dc05b73f8da912c8776241aa8ef7afb
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2024-03-29 02:33:10 +0530

    creating reusable component

--- template/index.js
+++ template/index.js
@@ -1,23 +1,49 @@
-import { File } from '@asyncapi/generator-react-sdk';
+import { File, Text } from '@asyncapi/generator-react-sdk'
+import { TopicFunction } from '../components/TopicFunction'
 
-// notice that now the template not only gets the instance of parsed AsyncAPI document but also the parameters
 export default function ({ asyncapi, params }) {
- 
-  return (
-    <File name="client.py">
-      {`import paho.mqtt.client as mqtt
+    return (
+        <File name="client.py">
+            <Text newLines={2}>import paho.mqtt.client as mqtt</Text>
 
-mqttBroker = "${asyncapi.servers().get(params.server).url()}"
+            <Text newLines={2}>mqttBroker = "{asyncapi.servers().get(params.server).url()}"</Text>
 
-class TemperatureServiceClient:
-    def __init__(self):
-        self.client = mqtt.Client()
-        self.client.connect(mqttBroker)
+            <Text newLines={2}>class {asyncapi.info().title().replaceAll(' ', '')}Client:</Text>
 
+            <Text indent={2} newLines={2}>
+                {`def __init__(self):
+            self.client = mqtt.Client()
+            self.client.connect(mqttBroker)`}
+            </Text>
 
-    def sendTemperatureChange(self, id):
-        topic = "temperature/changed"
-        self.client.publish(topic, id)`}
-    </File>
-  )
+            <Text indent={2}>
+                <TopicFunction channels={asyncapi.channels().filterByReceive()} />
+            </Text>
+        </File>
+    )
 }
+
+
+// import { File } from '@asyncapi/generator-react-sdk';
+
+// // notice that now the template not only gets the instance of parsed AsyncAPI document but also the parameters
+// export default function ({ asyncapi, params }) {
+ 
+//   return (
+//     <File name="client.py">
+//       {`import paho.mqtt.client as mqtt
+
+// mqttBroker = "${asyncapi.servers().get(params.server).url()}"
+
+// class TemperatureServiceClient:
+//     def __init__(self):
+//         self.client = mqtt.Client()
+//         self.client.connect(mqttBroker)
+
+
+//     def sendTemperatureChange(self, id):
+//         topic = "temperature/changed"
+//         self.client.publish(topic, id)`}
+//     </File>
+//   )
+// }
