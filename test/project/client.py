64bb6dbcf867a817b051a378de66eb18e3d8fe01
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2024-03-29 02:40:37 +0530

    complete resuable component

--- test/project/client.py
+++ test/project/client.py
@@ -3,11 +3,12 @@ import paho.mqtt.client as mqtt
 mqttBroker = "test.mosquitto.org"
 
 class TemperatureServiceClient:
-    def __init__(self):
-        self.client = mqtt.Client()
-        self.client.connect(mqttBroker)
 
+  def __init__(self):
+              self.client = mqtt.Client()
+              self.client.connect(mqttBroker)
 
-    def sendTemperatureChange(self, id):
-        topic = "temperature/changed"
-        self.client.publish(topic, id)
+  def sendTemperatureChange(self, id):
+          topic = "temperature/changed"
+          self.client.publish(topic, id)
+
